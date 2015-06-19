9e9a6c9112180ff5e537ebd278dcb7177623fe7a
Jonny Scholes <jonnyscholes@gmail.com>
2015-06-19 11:30:48 +1000

    Use .plugin api.

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,11 @@ var reactify = require('reactify');
 gulp.task('default', ['css-and-scripts', 'html']);
 
 gulp.task('css-and-scripts', function () {
-  var b = browserify('src/index.js', { transform: [reactify] });
-
-  cssmodulesify(b, { o: 'dist/main.css' })
-    .bundle()
-    .pipe(source('index.js'))
-    .pipe(gulp.dest('dist/'));
+  return browserify('src/index.js', { transform: [reactify] })
+      .plugin('css-modulesify', { o: 'dist/main.css' })
+      .bundle()
+      .pipe(source('index.js'))
+      .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('html', function () {
