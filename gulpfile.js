dbff71f08485896c7fc237e45e06f8a758269fd6
Jonny Scholes <jonnyscholes@gmail.com>
2015-09-24 11:06:32 +1000

    Added postcss simpleVars and doiuse

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,34 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var cssmodulesify = require('css-modulesify');
 var reactify = require('reactify');
+var simpleVars = require('postcss-simple-vars');
+var doiuse = require('doiuse');
+
+var cssVariables = require('./src/components/example/vars.js');
 
 gulp.task('default', ['css-and-scripts', 'html']);
 
 gulp.task('css-and-scripts', function () {
+  var postCSSPlugins = [
+		'postcss-modules-local-by-default',
+		'postcss-modules-extract-imports',
+		'postcss-modules-scope',
+		simpleVars({
+			variables: cssVariables
+		}),
+    doiuse({
+			browsers:['ie >= 9', '> 1%', 'last 2 versions'],
+			onFeatureUsage: function(usageInfo) {
+				logStatsPretty(usageInfo.message);
+			}
+		})
+  ];
+
   return browserify('src/index.js', { transform: [reactify] })
-      .plugin('css-modulesify', { o: 'dist/main.css' })
+      .plugin('css-modulesify', {
+        o: 'dist/main.css',
+        use: postCSSPlugins
+      })
       .bundle()
       .pipe(source('index.js'))
       .pipe(gulp.dest('dist/'));
